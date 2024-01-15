d0307f4919bf4c2ba250732c5d5a42bf0e8899d3
CharyWells <wellingtoncharlottenaaodarley@gmaail.com>
2024-01-15 23:46:51 +0000

    Added new files

---
+++ 6-main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * modify_my_param - set the integer to 402
+ * @m: a pointer the integer we want to set to 402
+ *
+ * Return: nothing
+ */
+void modify_my_parameter(int *m)
+{
+  printf("The value of 'm': %p\n",m);
+  printf("Addresses of 'm': %p \n",&m);
+  *m = 402;
+
+
+}
+/**
+ * main -how to change the value of a variable from outside the function
+ * it is declared in,using a pointer.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  int n;
+  int *p;
+  p = &n;
+  n = 98;
+  printf("Value of 'n' before the call: %d\n", n);
+  printf("Address of 'n': %p\n", &n);
+  printf("Value of 'p': %p\n",p);
+  return (0);
+  
+}
